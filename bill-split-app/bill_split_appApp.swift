cb205aaed3a83d0ca1480eadb79008b61efbcc44
Arnav Pratap Chaudhry <arnavpchaudhry3@gmail.com>
2023-07-22 03:52:25 +0530

    Initial Commit

---
+++ bill-split-app/bill_split_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bill_split_appApp.swift
+//  bill-split-app
+//
+//  Created by Arnav on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct bill_split_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
